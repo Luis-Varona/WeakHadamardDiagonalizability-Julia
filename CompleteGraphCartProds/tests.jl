2afad5956392eb57efc92bb663e40de65e88d89d
Luis Miguel Varona <lbvarona@mta.ca>
2024-07-19 00:56:25 -0300

    Ran more kGraph tests

--- CompleteGraphCartProds/tests.jl
+++ CompleteGraphCartProds/tests.jl
@@ -1,5 +1,5 @@
 using JLD, JLD2, FileIO
-#include("BFS_WHDiagonalizability/struct-WHDecomp_Set.jl")
+using FreqTables
 include("BFS_WHDiagonalizability/getall_WHDecomps.jl")
 
 ### Load K_2 and K_3 info
@@ -14,10 +14,16 @@ vecs_2n = load("../WHD-Julia-LargeFiles/K4-tests/K4_vecs.jld", "vecs_2n")
 bases_0 = load("../WHD-Julia-LargeFiles/K4-tests/K4-bases_0.jld", "bases_0")
 bases_n = load("../WHD-Julia-LargeFiles/K4-tests/K4-bases_n.jld", "bases_n")
 
-### Tests
-using FreqTables
+### Tests for K_2
+orthoRanks_K2 = freqtable(
+    [data.OrthoRank for data in decomp_info2.get_decomps()]
+)
+eigvecs_K2 = [data.Eigvecs for data in decomp_info2.get_decomps()]
+
+### Tests for K_3
 idxs = rand(1:189, 4)
 
-tests = [data.Eigvecs for data in decomp_info3.get_decomps()[idxs]]
-tests2 = [data.OrthoRank for data in decomp_info3.get_decomps()]
-tests2_freq = freqtable(tests2)
+orthoRanks_K3 = freqtable(
+    [data.OrthoRank for data in decomp_info3.get_decomps()]
+)
+eigvecs_K3 = [data.Eigvecs for data in decomp_info3.get_decomps()[idxs]]
