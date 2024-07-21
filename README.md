9b790c19f9cd1cfa85542be6a5f623e58a252660
Luis Miguel Varona <lbvarona@mta.ca>
2024-07-21 03:18:05 -0300

    Edited readme and WHD tests

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # WeaklyHadamardDiagonalizable-Julia
-An algorithm to tests simple, connected graphs for weak Hadamard diagonalizability.
+An algorithm to test simple, connected graphs for weak Hadamard diagonalizability.
 Can test any graph of order <= 13 in minutes (and many higher-order graphs as well).
 
-NOTE: Many of these functions require entrywise integer matrices with integral spectra
-as input, since this is a precondition for weak Hadamard diagonalizability.
+NOTE: Many functions require integer Laplacians with integral spectra as input, since this is a precondition for both {-1,0,1}- and {-1,1}-diagonalizability.
--- WHDGraphs/GraphStructs.jl
+++ WHDGraphs/GraphStructs.jl
@@ -1,14 +1,51 @@
-struct Eigendecomp
-    V::Matrix{Int64}
-    d::Vector{Int64}
-end
+module GraphStructs
+    using LinearAlgebra
+    using ExportAll
+    
+    struct WHDGraph
+        laplacian_matrix::Matrix{Int64}
+        band_ZeroOneNeg::Int64
+        # band_OneNeg::Int64 # do later
+        spectral_decomp::Eigen
+    end
+    
+    function Base.getproperty(g::WHDGraph, prop::Symbol)
+        attributes = (:laplacian_matrix, :band_ZeroOneNeg, :band_OneNeg, :spectral_decomp)
+        if prop in attributes
+            return getfield(g, prop)
+        elseif prop == :adjacency_matrix
+            L = g.laplacian_matrix
+            return Diagonal(L) - L
+        elseif prop == :order
+            return size(g.laplacian_matrix, 1)
+        elseif prop == :size
+            return convert(Int64, sum(g.adjacency_matrix)/2)
+        elseif prop == :density
+            n = g.order
+            return 2*g.size/(n*(n - 1))
+        elseif prop == :average_degree
+            l = diag(g.laplacian_matrix)
+            return sum(l)/length(l)
+        # elseif prop == :graph6_string
+            # return adjacency_to_graph6(g.adjacency_matrix)
+        end
+    end
+    
+    ###
+    # Porting from Python (not yet done):
+        # https://stackoverflow.com/questions/44532492/how-does-graph6-format-work
 
-struct WHDGraph
-    order::Int64
-    #graph6::String
-    adjacency::Matrix{Int64}
-    laplacian::Matrix{Int64}
-    BW_ZeroOneNeg::Int64
-    #BW_OneNeg::Int64 # do later
-    diagonalization::Eigendecomp
+    # function adjacency_to_graph6(A::Matrix{Int64})
+    #     elements_from_row = 0
+    #     bin_list = ""
+
+    #     for row in eachrow(A)
+    #         elements_from_row += 1
+    #         if elements_from_row == 1
+    #             continue
+    #         end
+    #         row_elements = pusheen
+    # end
+    
+    @exportAll()
 end
